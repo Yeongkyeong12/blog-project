3ff5f4c7a3a8f06aa6ad7686bbd0d7536284def2
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-04-11 21:20:52 +0900

    Feat: add getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
+import fs from "fs";
+import path from "path";
 import Head from "next/head";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <title>Dev Blog</title>
+        <title>My Blog</title>
       </Head>
 
       <h2>Hello</h2>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  // post directory에서 파일 가져오기
+  const files = fs.readdirSync(path.join("posts"));
+
+  // post directory에서 slug,frontmatter 가져오기
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    return {
+      slug,
+    };
+  });
+
+  console.log(posts);
+
+  return {
+    props: {
+      posts: "The Posts",
+    },
+  };
 }
---
+++ posts/test.md
@@ -0,0 +1,11 @@
+---
+title: "Test Post"
+date: "April 11 2022"
+excerpt: "excerpt section"
+cover_image: "/images/posts/img1.jpg"
+---
+
+##Heading Two
+
+- Item 1
+- Item 2
